09585640bf539de711748d4a60264ee85188b903
Sergio <sfvasquez96@gmail.com>
2026-09-05 10:14:12 -0600

    Fixed a bug with user sessions

--- src/proxy.js
+++ src/proxy.js
@@ -33,6 +33,12 @@ export async function proxy(request) {
     request.nextUrl.pathname === "/login" ||
     request.nextUrl.pathname === "/signup";
 
+  if (!user && !isPublicRoute) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
   if (user && isPublicRoute) {
     const url = request.nextUrl.clone();
     url.pathname = "/";
